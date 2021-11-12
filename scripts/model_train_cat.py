899c972a35c0ecf6448b74de1a942bca57099ebe
mactat <maciektatarski@gmail.com>
2021-11-12 14:18:09 +0000

    Tested on HPC

--- scripts/model_train_cat.py
+++ scripts/model_train_cat.py
@@ -17,6 +17,7 @@ import random
 from model import *
 from datasetLoad import *
 
+divider = "----------------------------------------------------------------\n"
 min_size = (33, 42)
 avg_size = (360, 360)
 input_size = (180, 180)
@@ -42,7 +43,7 @@ train_size = int(0.8 * len(dataset_train))
 test_size = len(dataset_train) - train_size
 dataset_train, dataset_validation = torch.utils.data.random_split(dataset_train, [train_size, test_size])
 
-print(f"DataSet loaded!!!\nTrain set size: {len(dataset_train)}\nValidation size: {len(dataset_validation)}\nTest set size {len(dataset_test)}")
+print(f"DataSet loaded!\n\nTrain set size: {len(dataset_train)}\nValidation size: {len(dataset_validation)}\nTest set size {len(dataset_test)}\n")
 
 batch_size = 5
 
@@ -51,6 +52,8 @@ dataloader_validation = DataLoader(dataset_validation, batch_size=batch_size, sh
 dataloader_test = DataLoader(dataset_test, batch_size=batch_size, shuffle=True)
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+print(f"Device: {device}\n")
 
 rae = ResolutionAutoencoder()
 rae = rae.to(device)
@@ -67,7 +70,7 @@ summary(rae, (3, 180, 180), 1)
 val_loss_avg = []
 train_loss_avg = []
 for t in range(num_epochs):
-    print(f"Epoch {t+1}\n-------------------------------")
+    print(f"Epoch {t+1}\n{divider}")
     train_loss = train(dataloader_train, rae, criterion, optimizer)
     val_loss = validate(dataloader_validation, rae, criterion)
     
@@ -76,7 +79,7 @@ for t in range(num_epochs):
     
     print(f"Train loss: {train_loss:>8f}\nTest Error: {val_loss:>8f} \n")
     
-print("Done!")
+print(f"\n{divider}Done!")
 
 test_loss = validate(dataloader_test, rae, criterion)
 print('average reconstruction error: %f' % (test_loss))
