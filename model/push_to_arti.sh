0a5746adfd68ff4d3fd85521d038283c337c232c
Maciej Tatarski <maciej.tatarski@a4bee.com>
2021-11-22 08:07:58 +0100

    better dir struct

--- model/push_to_arti.sh
+++ model/push_to_arti.sh
@@ -5,13 +5,14 @@
 # 3 -> path to log file 
 
 # push model itself
-curl -umaciektatarski@gmail.com:APAntTipeLPUsdB53fm4YkcoMtA -T ./$2.pth "https://dlmodels.jfrog.io/artifactory/iqiwa-models/$1/$2.pth"
+curl -umaciektatarski@gmail.com:APAntTipeLPUsdB53fm4YkcoMtA -T ./$2.pth "https://dlmodels.jfrog.io/artifactory/iqiwa-models/$1/$2/model.pth"
 
 # push logs
-curl -umaciektatarski@gmail.com:APAntTipeLPUsdB53fm4YkcoMtA -T ./$3.log "https://dlmodels.jfrog.io/artifactory/iqiwa-models/$1/$2.log" || echo "logs not found"
+curl -umaciektatarski@gmail.com:APAntTipeLPUsdB53fm4YkcoMtA -T ./$3.log "https://dlmodels.jfrog.io/artifactory/iqiwa-models/$1/$2/logs.log" || echo "logs not found"
 
 # push model file
-curl -umaciektatarski@gmail.com:APAntTipeLPUsdB53fm4YkcoMtA -T ../scripts/$3.py "https://dlmodels.jfrog.io/artifactory/iqiwa-models/$1/$2.py" || echo "logs not found"
+curl -umaciektatarski@gmail.com:APAntTipeLPUsdB53fm4YkcoMtA -T ../scripts/$3.py "https://dlmodels.jfrog.io/artifactory/iqiwa-models/$1/$2/model.py" || echo "model definition not found"
+
 # push example phots
 
 # to be done
