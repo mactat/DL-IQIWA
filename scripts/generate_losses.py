e996906c65abf04cd288a5a19fa1f22fef994412
jurek-n <62500677+jurek-n@users.noreply.github.com>
2021-11-29 12:15:38 +0100

    Compute losses

---
+++ scripts/generate_losses.py
@@ -0,0 +1,87 @@
+from torch.nn.functional import interpolate
+from torchvision.transforms.functional import InterpolationMode
+from dataset_load import *
+from torch.utils.data import  DataLoader
+import importlib
+import torch.nn as nn
+from kornia.losses.ssim import SSIMLoss
+from kornia.losses.psnr import PSNRLoss, psnr_loss
+
+
+input_size = (90, 90)
+avg_size = (360, 360)
+
+
+transform = transforms.Compose(
+    [transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+
+transform_up = transforms.Compose(
+    [transforms.ToPILImage(),
+     transforms.Resize(avg_size),
+     transforms.ToTensor()]
+     )
+
+transform_down = transforms.Compose(
+    [transforms.CenterCrop(size=avg_size)]
+    )
+
+dataset_test = CatAndDogDataset('./data/test1/', transform_upscale=transform_up, transform_downscale=transform_down)
+
+dataloader_test = DataLoader(dataset_test, batch_size=1, shuffle=True)
+
+try:
+    bestModelLib = importlib.import_module("resnet")
+    transposeModelLib = importlib.import_module("tnn")
+
+    bestModel = bestModelLib.Model
+    transposeModel = transposeModelLib.Model
+
+    bestModelLib.load_state_dict(torch.load("resnet.pth"))
+    transposeModelLib.load_state_dict(torch.load("tnn.pth"))
+except:
+    raise Exception('Unable to import model lib!') 
+
+
+mseLoss = nn.MSELoss()
+psnrLoss = PSNRLoss()
+ssimLoss = SSIMLoss(5)
+
+MSE_losses  = [0, 0, 0, 0]
+SSIM_losses = [0, 0, 0, 0]
+PSNR_losses = [0, 0, 0, 0]
+
+resize_t = transforms.Resize(input_size)
+resize_t_180 = transforms.Resize((180, 180))
+resize_t_nearest = transforms.Resize((360, 360), interpolation=InterpolationMode.NEAREST)
+resize_t_bicubic = transforms.Resize((360, 360), interpolation=InterpolationMode.BICUBIC)
+for i, image_batch in enumerate(dataloader_test):
+    scaled_down_180 = resize_t_180(image_batch)
+    scaled_down_90 = resize_t(image_batch)
+
+    scaled_up_nearest = resize_t_nearest(scaled_down_90)
+    scaled_up_bicubic = resize_t_bicubic(scaled_down_90)
+    scaled_up_bestModel = bestModel(scaled_down_90)
+    scaled_up_transposeModel = transposeModel(scaled_down_180)
+
+    MSE_losses[0] +=  mseLoss(scaled_up_nearest, image_batch)
+    MSE_losses[1] +=  mseLoss(scaled_up_bicubic, image_batch)
+    MSE_losses[2] +=  mseLoss(scaled_up_bestModel, image_batch)
+    MSE_losses[3] +=  mseLoss(scaled_up_transposeModel, image_batch)
+
+    SSIM_losses[0] += ssimLoss(scaled_up_nearest, image_batch)
+    SSIM_losses[1] += ssimLoss(scaled_up_bicubic, image_batch)
+    SSIM_losses[2] += ssimLoss(scaled_up_bestModel, image_batch)
+    SSIM_losses[3] += ssimLoss(scaled_up_transposeModel, image_batch)
+
+    PSNR_losses[0] += psnrLoss(scaled_up_nearest, image_batch)
+    PSNR_losses[1] += psnrLoss(scaled_up_bicubic, image_batch)
+    PSNR_losses[2] += psnrLoss(scaled_up_bestModel, image_batch)
+    PSNR_losses[3] += psnrLoss(scaled_up_transposeModel, image_batch)
+
+
+for j in range(len(MSE_losses)):
+    MSE_losses[i] = MSE_losses[i]/len(dataloader_test) 
+    PSNR_losses[i] = MSE_losses[i]/len(dataloader_test) 
+    SSIM_losses[i] = MSE_losses[i]/len(dataloader_test)
+
---
+++ scripts/tnn.py
@@ -0,0 +1,78 @@
+import torch.nn.functional as F
+from torchvision import transforms
+import torch
+import torch.nn as nn
+from datetime import datetime
+
+input_size = (180, 180)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()                                                               # input image size: 180 x 180
+        self.conv1 = nn.ConvTranspose2d(in_channels=3, out_channels=10, kernel_size=3, stride=1, padding=1)    # out image size: 180 x180                                                       # out image size:  90 x 90
+        self.conv2 = nn.ConvTranspose2d(in_channels=10, out_channels=20, kernel_size=3, stride=2, padding=1)   # out image size:  359 x 359
+        self.conv3 = nn.ConvTranspose2d(in_channels=20, out_channels=3, kernel_size=4, stride=1, padding=1)   # out image size: 360 x 360
+
+        self.criterion = nn.MSELoss()
+            
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = torch.sigmoid(self.conv3(x))
+        return x
+
+def train(dataloader, model, loss_fn, optimizer, transform=transforms.Resize((180, 180))):
+    size = len(dataloader.dataset)
+    model.train()
+    train_loss = []
+    for batch, (original_image_batch) in enumerate(dataloader):
+        if transform:
+            t_image_batch = transform(original_image_batch)
+
+        t_image_batch = t_image_batch.to(device)
+        original_image_batch = original_image_batch.to(device)
+        # Compute prediction error
+        pred = model(t_image_batch)
+        loss = loss_fn(pred, original_image_batch)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if batch % 1000 == 0:
+            loss, current = loss.item(), batch * len(t_image_batch)
+            train_loss.append(loss)
+            print(f"train loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+    return (sum(train_loss))/len(train_loss)
+
+
+def validate(dataloader, model, loss_fn, transform=transforms.Resize((180, 180))):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for original_image_batch in dataloader:
+            if transform:
+                t_image_batch = transform(original_image_batch)
+            t_image_batch = t_image_batch.to(device)
+            original_image_batch = original_image_batch.to(device)
+            pred = model(t_image_batch)
+            test_loss += loss_fn(pred, original_image_batch).item()
+    test_loss /= num_batches
+    
+    return test_loss
+
+def save_model(name, model, path='../model/'):
+    now = datetime.now()
+    dt_string = now.strftime("%d%m%Y_%H_%M")
+    model_name = name + "_" + dt_string
+    full_path = path+model_name+".pth"
+    torch.save(model.state_dict(), full_path)
+    
+    print(f"Succesfully saved model!\nPath: {path}\nName: {model_name}\n")
+    
+    return full_path
